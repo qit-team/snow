df794c102700161cb77a789e6e4d911c8176c367
tinson <hetiansu@qudian.com>
2019-07-22 13:28:54 +0800

    upd:template go.mod

--- tool/template_project.go
+++ tool/template_project.go
@@ -9,16 +9,13 @@ go 1.12
 
 require (
 	github.com/BurntSushi/toml v0.3.1
-	github.com/fatih/color v1.7.0
 	github.com/gin-gonic/gin v1.4.0
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/go-xorm/core v0.6.2
 	github.com/go-xorm/xorm v0.7.3
-	github.com/mattn/go-colorable v0.1.2 // indirect
 	github.com/qit-team/snow-core v0.1.1
 	github.com/qit-team/work v0.3.3
 	github.com/robfig/cron v1.2.0
-	github.com/urfave/cli v1.20.0
 )`
 
 	_tplMain = `package main
