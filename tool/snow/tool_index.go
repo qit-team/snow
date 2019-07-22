009bbb9a3ecadc645d98c388be01589ce3137b99
tinson <hetiansu@qudian.com>
2019-07-22 14:41:16 +0800

    fix:snow upgrade

--- tool/snow/tool_index.go
+++ tool/snow/tool_index.go
@@ -7,7 +7,7 @@ var toolIndexs = []*Tool{
 		Name:      "snow",
 		Alias:     "snow",
 		BuildTime: time.Date(2019, 7, 19, 0, 0, 0, 0, time.Local),
-		Install:   "go get -u github.com/qit-team/snow/tool",
+		Install:   "go get -u github.com/qit-team/snow/tool/snow",
 		Summary:   "snow工具集本体",
 		Platform:  []string{"darwin", "linux", "windows"},
 		Author:    "snow",
