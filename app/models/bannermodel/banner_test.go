86bd88c470c1b9949572e157ccb6c2cd3eaac81a
hetiansu5 <541919050@qq.com>
2019-07-25 16:49:14 +0800

    fix:错删model恢复

---
+++ app/models/bannermodel/banner_test.go
@@ -0,0 +1,53 @@
+package bannermodel
+
+import (
+	"fmt"
+	"testing"
+	"github.com/qit-team/snow-core/config"
+	"github.com/qit-team/snow-core/db"
+	"github.com/qit-team/snow-core/utils"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func init() {
+	m := config.DbBaseConfig{
+		Host:     "127.0.0.1",
+		Port:     3306,
+		User:     "root",
+		Password: "123456",
+		DBName:   "test",
+	}
+	dbConf := config.DbConfig{
+		Driver: "mysql",
+		Master: m,
+	}
+
+	err := db.Pr.Register("db", dbConf, true)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func TestGetOne(t *testing.T) {
+	bannerModel := GetInstance()
+	banner := new(Banner)
+	res, err := bannerModel.GetOne(1, banner)
+	if err != nil {
+		t.Error(err)
+	} else if res != true {
+		t.Error("missing bannner data")
+	} else if banner.Id == 0 {
+		t.Error("missing bannner data")
+	}
+	fmt.Println(utils.JsonEncode(banner))
+}
+
+func TestGetList(t *testing.T) {
+	bannerModel := GetInstance()
+	banners := make([]*Banner, 0)
+	err := bannerModel.GetList(&banners, "pid >= ?", []interface{}{1}, []int{10}, "status desc, id desc")
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(utils.JsonEncode(banners))
+}
